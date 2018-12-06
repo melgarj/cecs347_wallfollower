a45374a23582d965d88a30fa1a2e83a9b2ef1274
stevensfans <1anh21@gmail.com>
2018-12-06 15:33:21 -0800

    car changes with ir

--- 347_Project3/Period_Measurement_Timeout.c
+++ 347_Project3/Period_Measurement_Timeout.c
@@ -21,10 +21,11 @@
  */
 
 #include <stdint.h>
-#include "SysTick.h"
 #include "tm4c123gh6pm.h"
 #include "PLL.h"
 #include "Nokia5110.h"
+#include "ADCSWTrigger.h"
+#include "states.h"
 
 #define MC_LEN 0.0625      // length of one machine cyce in microsecond for 16MHz clock
 #define SOUND_SPEED 0.0343 // centimeter per micro-second
@@ -35,6 +36,14 @@ void Timer1_Init(void);
 void WaitForInterrupt(void);  // low power mode
 void EnableInterrupts(void);
 void PortD_Init(void);
+void init_PortF(void); 
+void SysTick_Init(void); 
+void forwardDirection(void);
+void SysTick_Wait(unsigned long delay);
+void SysTick_Wait1us(unsigned long delay); 
+
+	unsigned int testa, testb, test1, test2; 
+	unsigned char testButton; 
 
 uint32_t period=0;
 uint8_t done=0, timeout=0;
@@ -49,7 +58,7 @@ uint8_t OutOfRange = 0;
 
 // following variables are for for the ain use for the ir
 unsigned long ain1, ain2, ain3, dutyCycle;
-char sample = 0; 
+char sample=0;  
 
 ///////////////////ADC Stuff///////////////////
 int adcTable[] = {4095, 3050, 1980, 1370, 950, 830, 730, 650, 570, 530, 460, 390, 330, 300, 0};
@@ -68,6 +77,11 @@ int f;
 float dist1, dist2;
 float DC;
 
+unsigned int period_pwm = 5000; 
+//unsigned int lDuty, rDuty; 
+unsigned int left = 4960, right  = 4960; // initially moving wheels 
+int array2[] = {0,0}; 
+
 //======================================================================
 //DEFINITIONS FOR PWM
 #define SYSCTL_RCC_USEPWMDIV  0x00100000 // Enable PWM Clock Divisor
@@ -78,7 +92,7 @@ float DC;
 #define LEFTBACKWARD 			 		(*((volatile unsigned long *)0x40007020)) //PD3
 #define RIGHTFORWARD			   	(*((volatile unsigned long *)0x40007100)) //Pd6
 #define RIGHTBACKWARD 				(*((volatile unsigned long *)0x40007200)) //Pd7
-	
+
 #define LED										(*((volatile unsigned long*)0x40025038)) // PF3-1
 
 #define RED 0x02;
@@ -93,8 +107,13 @@ float DC;
 #define IR_ON 0x1
 #define IR_OFF 0x0
 
+#define leftWheel   0x00
+#define rightWheel  0x01
+
+/*
 char direction;
 unsigned char s;
+
 
 struct State{
 	unsigned long LPWM;
@@ -111,50 +130,88 @@ typedef const struct State stateType;
 
 stateType fsm[4]={
 	// Straight
-	{4998, 4998, {STRAIGHT, RIGHT, LEFT, STOP}},
+	{1250, 1250, {STRAIGHT, RIGHT, LEFT, STOP}},
 	
 	// Right
-	{4998, 1250, {STRAIGHT, RIGHT, LEFT, STOP}},
+	{29000, 1250, {STRAIGHT, RIGHT, LEFT, STOP}},
 	
 	// Left
 	{1250, 4998, {STRAIGHT, RIGHT, LEFT, STOP}},
 	
 	// Stop
-	{1250, 1250, {STRAIGHT, RIGHT, LEFT, STOP}}
+	{35000, 35000, {STRAIGHT, RIGHT, LEFT, STOP}}
 };
-
-
-
+*/
 
 int main(void){
-	period = 5000;
-	dutyCycle = 4998;
-	LEFTFORWARD   = 0xFF;
-	LEFTBACKWARD  = 0x00;					
-	RIGHTFORWARD  = 0xFF;
-	RIGHTBACKWARD = 0x00; 
-//  PLL_Init(Bus80MHz);               // 80 MHz clock
+
+  //PLL_Init();               // 80 MHz clock
+	ADC_Init298();
 	EnableInterrupts();
-	//PLL_Init();
+	PLL_Init();
 	Nokia5110_Init();
 	Nokia5110_Clear();
 	SysTick_Init();         // use default 16MHz clock
+  init_PortF();
 	PortD_Init();
+	forwardDirection();
+	
   Timer0_Init();          // initialize timer0A
   Timer1_Init();
-  ain3 = 4998;
-  while(1){
+	
+  //ain3 = 4998;
+	LED = BLUE; 
+	
+	while(1){
+
+		/*
+		if(dist1 <20){
+		left = 	Right(leftWheel);
+		right =  Right(rightWheel); 
+
+		PWM1_0_CMPA_R = right - 1;
+		PWM1_0_CMPB_R = left  - 1;
+		LED = RED; 
+		}
+		
+		else if (dist2 <20){
+		left = 	Left(leftWheel);
+		right =  Left(rightWheel); 
+
+		PWM1_0_CMPA_R = right - 1;
+		PWM1_0_CMPB_R = left  - 1;
+		LED = GREEN; 
+		}
+		
+		else if(dist2 > 20 && dist1 > 20){
+		left = 	Forward(leftWheel);
+		right =  Forward(rightWheel);
+		
+		PWM1_0_CMPA_R = right - 1;
+		PWM1_0_CMPB_R = left  - 1;
+		
+		LED = 0x0C; 
+		}
 		
 		
-		/*       	PWM           */
-		s = fsm[s].NS[STRAIGHT];
-		PWM1_0_CMPA_R = fsm[s].LPWM;
-		PWM1_0_CMPB_R = fsm[s].RPWM;
+	  if(dist2 < 20 && dist1 <20) {
+		left = 	Stop(leftWheel);
+		right =  Stop(rightWheel);
+		
+		PWM1_0_CMPA_R = right - 1;
+		PWM1_0_CMPB_R = left  - 1;
+		
+		LED = 0x0A; 
+		}
+		*/
 		
 		/******************************/
 		
 		
 		Nokia5110_SetCursor(0,0);
+	 
+		
+		
 		GPIO_PORTB_DATA_R &= ~0x80; // send low to trigger
 		SysTick_Wait1us(2);
 		GPIO_PORTB_DATA_R |= 0x80; // send high to trigger
@@ -171,21 +228,23 @@ int main(void){
     TIMER1_TAILR_R = MAX_DURATION;// reload value
 		TIMER1_CTL_R = 0x00000001;    // enable TIMER1A
 		
+		 Nokia5110_OutString("testtest");
+		 
 		// Use general purpose timer input edge mode 16 bits count, 
 		// detectable range: (65535*62.58*10^(-3)*0.0343)/2=70.2cm
 		// Notice that the detect range for HC - SR04 ultrasonic sensor is 400cm
     // Since our application only need to detect obstcle within 70cm, 
     // 16 bits count is good enough for us.		
 		
-    while ((!done)&&(!timeout));
+    while ((!done)&&(!timeout)){
 	  TIMER0_CTL_R = 0x00000000;    // disable TIMER0A 
 	  TIMER1_CTL_R = 0x00000000;    // disable TIMER1A 
     TIMER0_IMR_R = 0x00000000;    // disable interrupt
     TIMER1_IMR_R = 0x00000000;    // disable interrupt
 		
 		if (done) {
-			// The speed of sound is approximately 340 meters per second, 
-			// or  .0343 c/µS.
+			// The speed of sound is approximately 340 meters per second, 
+			// or  .0343 c/µS.
       // Distance = (duration * 0.0343)/2;
 		  distance = (period*MC_LEN*SOUND_SPEED)/2;	
 			OutOfRange = 0;
@@ -193,16 +252,104 @@ int main(void){
 			Nokia5110_SetCursor(0,0);
 			Nokia5110_OutUDec(distance);
 		}
+		/*
 		else { // out of range			
 		  distance = 0;
 			OutOfRange = 1;
 			Nokia5110_SetCursor(0,0);
 			Nokia5110_OutString(" OOR ");
-		}
+		}*/
 		//Nokia5110_OutString("test");
 		first = 0;
 		done = 0;
     timeout	= 0;		
+}
+//=========================================================================================		
+/*
+    if(sample){
+			sample = 0;	
+			ADC_In298(&ain1, &ain2, &ain3); // Ensure sampler works
+			GPIO_PORTF_DATA_R ^= 0x04;
+			
+			// Update Motors
+			//dutyCycle = ain3-1;
+			//PWM1_0_CMPA_R = dutyCycle;
+			//PWM1_0_CMPB_R = dutyCycle;
+			
+			//Update Sensors
+			// Find distance
+		for(i = 0; i < 15; i = i + 1){
+			if(ain1 > adcTable[i]){
+				break;
+			}
+			else{
+				a = adcTable[i+1];
+				ia = i+1;
+			}
+		}
+		
+		for(f = 0; f < 15; f = f + 1){
+			if(ain1 < adcTable[f]){
+				b = adcTable[f];
+				ib = f;
+			}
+			else {
+				break;
+			}
+		}
+		 m = b - a;
+		 l = b - ain1;
+		lm = l / m ;
+		
+		dist1 = distTable[ib] + (lm * 5);
+		
+		// Find distance
+		for(i = 0; i < 15; i = i + 1){
+			if(ain2 > adcTable[i]){
+				break;
+			}
+			else{
+				a = adcTable[i+1];
+				ia = i+1;
+			}
+		}
+		
+		for(f = 0; f < 15; f = f + 1){
+			if(ain2 < adcTable[f]){
+				b = adcTable[f];
+				ib = f;
+			}
+			else {
+				break;
+			}
+		}
+		 m = b - a;
+		 l = b - ain2;
+		lm = l / m ;
+		
+		dist2 = distTable[ib] + (lm * 5);
+		
+		DC = (ain3*100)/4095;
+		DC = 100 - DC; 
+		
+			//Update LCD
+			Nokia5110_SetCursor(0,0);
+			Nokia5110_OutString("Duty Cycle:");
+			Nokia5110_SetCursor(0,1);
+			Nokia5110_OutUDec(DC);
+			Nokia5110_SetCursor(0,2);
+			Nokia5110_OutString("Sensor 1:");
+			Nokia5110_SetCursor(0,3);
+			if(dist1 > 60) Nokia5110_OutString("  OOR");
+			else Nokia5110_OutUDec(dist1);
+			Nokia5110_SetCursor(0,4);
+			Nokia5110_OutString("Sensor 2:");
+			Nokia5110_SetCursor(0,5);
+			if (dist2 >60) Nokia5110_OutString("  OOR");
+			else Nokia5110_OutUDec(dist2);
+
+		}
+		*/
 	}
 }
 // ***************** Timer0_Init ****************
@@ -308,16 +455,124 @@ void PortD_Init(void){
 	SYSCTL_RCC_R &= ~SYSCTL_RCC_PWMDIV_M;  // clear the PWM divider field
 	SYSCTL_RCC_R += SYSCTL_RCC_PWMDIV_2;   // configure for /2 diveder
 
-	// STEP 6: confiure genertor countdown mode
+	// STEP 6: confiure genertor countdown mode PWM1_0 is for PD0
 	PWM1_0_CTL_R  &= ~0xFFFFFFFF;
 	PWM1_0_GENA_R |= 0x0000008C;
 	PWM1_0_GENB_R |= 0x0000080C;
 	
-	PWM1_0_LOAD_R = period - 1;           // STEP 7 : set period
-  PWM1_0_CMPA_R = ain3 - 1;    		// STEP 8: set duty cycle
-  PWM1_0_CMPB_R =	ain3 - 1;
+	
+	PWM1_0_LOAD_R = period_pwm - 1;    // STEP 7 : set period
+	
+  PWM1_0_CMPA_R =  right - 1;    	     // STEP 8: set duty cycle right
+  PWM1_0_CMPB_R =  left - 1; 				//left 
+	
+	testa = PWM1_0_CMPA_R; 
+	testb = PWM1_0_CMPB_R; 
+	
   PWM1_0_CTL_R  |= 0x00000001;   	  // STEP 9: start the M1PWM5 generator
   PWM1_ENABLE_R |= 0x00000003;			// STEP 10: enable   M1PWM0-1 outputs
 }
+
+void init_PortF(void){ 
+	volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
+	delay = SYSCTL_RCGC2_R;
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
+	GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF3-0 
+	GPIO_PORTF_DIR_R = 0x0E;          // 5)PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R &= ~0x1F;  //     disable alt funct on PF4
+  GPIO_PORTF_DEN_R |= 0x1F;     //     enable digital I/O on PF4  
+  GPIO_PORTF_PCTL_R &= ~0x000FFFFF; // configure PF4 as GPIO
+  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x11;     //     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x11;     // (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x11;    //     PF4 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x11;    //     PF4 falling edge event
+  GPIO_PORTF_ICR_R = 0x11;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x11;      // (f) arm interrupt on PF4
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R |= 0x40000000;      // (h) enable interrupt 30 in NVIC
+}
+
+
+void GPIOPortF_Handler(void){
+	
+	if ((GPIO_PORTF_RIS_R & 0x10)){ // sw1 is pressed
+		GPIO_PORTF_ICR_R = 0x10;
+	left = 	Left(leftWheel);
+	right =  Left(rightWheel); 
+
+	PWM1_0_CMPA_R = right - 1;
+	PWM1_0_CMPB_R = left  - 1;
+		LED = RED; 
+		
+
+	}
+	
+	if ((GPIO_PORTF_RIS_R & 0x01)){ // sw2 is pressed
+		GPIO_PORTF_ICR_R = 0x01;
+	left = 	Right(leftWheel);
+	right =  Right(rightWheel);
+
+	PWM1_0_CMPA_R = right - 1;
+	PWM1_0_CMPB_R = left  - 1;
+			
+		LED = GREEN; 
+	}
+}
+
+// Time delay using busy wait.
+// The delay parameter is in units of the core clock. (units of 62.5 nsec for 80 MHz clock)
+void SysTick_Wait(unsigned long delay){
+  volatile unsigned long elapsedTime;
+  unsigned long startTime = NVIC_ST_CURRENT_R;
+  do{
+    elapsedTime = (startTime-NVIC_ST_CURRENT_R)&0x00FFFFFF;
+  }
+  while(elapsedTime <= delay);
+}
+
+// Time delay using busy wait.
+// This assumes 16 MHz system clock.
+void SysTick_Wait1us(unsigned long delay){
+  unsigned long i;
+  for(i=0; i<delay; i++){
+    SysTick_Wait(16);  // wait 1us (assumes 16 MHz clock)
+  }
+}
+
+// Can sample at 20 or 10 Hz also
+void SysTick_Init(void){
+	NVIC_ST_CTRL_R = 0;         // disable SysTick during setup
+  NVIC_ST_RELOAD_R = 2000000-1;// reload value
+  NVIC_ST_CURRENT_R = 0;      // any write to current clears it
+  NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x40000000; // priority 2
+                              // enable SysTick with core clock and interrupts
+  NVIC_ST_CTRL_R = 0x07;
+}
+
+void SysTick_Handler(){
+	sample = 1;
+}
+
+void forwardDirection(void){
+	LEFTFORWARD   = 0xFF;																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																															
+	LEFTBACKWARD  = 0x00;					
+	RIGHTFORWARD  = 0xFF;
+	RIGHTBACKWARD = 0x00; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ 347_Project3/states.c
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "states.h"
+	
+char direction;
+unsigned char s;
+unsigned int lDuty, rDuty; 
+int array[] = {0,0}; 
+
+struct State{
+	unsigned long LPWM;
+	unsigned long RPWM;
+	
+	uint32_t NS[4];
+};
+
+typedef const struct State stateType;
+#define STRAIGHT 0
+#define RIGHT 1
+#define LEFT 2
+#define STOP 3
+
+stateType fsm[4]={
+	// Straight
+	{30, 30, {STRAIGHT, RIGHT, LEFT, STOP}},
+	
+	// Right
+	{30, 2060, {STRAIGHT, RIGHT, LEFT, STOP}},
+	
+	// Left
+	{2060, 30, {STRAIGHT, RIGHT, LEFT, STOP}},
+	
+	// Stop
+	{4960, 4960, {STRAIGHT, RIGHT, LEFT, STOP}}
+};
+
+int Forward(char wheel){
+		s = fsm[s].NS[STRAIGHT];
+		lDuty = fsm[s].LPWM; 
+		rDuty = fsm[s].RPWM; 
+		array[0] = lDuty;
+		array[1] = rDuty; 
+
+		return array[wheel]; 
+}
+	
+int Right(char wheel){
+		s = fsm[s].NS[RIGHT];
+		lDuty = fsm[s].LPWM; 
+		rDuty = fsm[s].RPWM; 
+		array[0] = lDuty;
+		array[1] = rDuty; 
+
+		return array[wheel]; 
+}
+	
+int Left(char wheel){
+		s = fsm[s].NS[LEFT];
+		lDuty = fsm[s].LPWM; 
+		rDuty = fsm[s].RPWM; 
+		array[0] = lDuty;
+		array[1] = rDuty; 
+
+		return array[wheel]; 
+}
+	
+int Stop(char wheel){
+		s = fsm[s].NS[STOP];
+		lDuty = fsm[s].LPWM; 
+		rDuty = fsm[s].RPWM; 
+		array[0] = lDuty;
+		array[1] = rDuty; 
+
+		return array[wheel]; 
+}
+	
+
+
+	
